5adf53957a96b3479fcd017bbad7bae29d9d1e3f
AbhisheekKoshta <abhi771991@gmail.com>
2018-04-02 18:08:27 +0530

    New page format added

--- Graphs.py
+++ Graphs.py
@@ -5,20 +5,24 @@ def num_graph(Heading,Num,Unit,color):
         import matplotlib.pyplot as plt
         fnt = "felix titling"
         plt.rcParams["font.family"] = fnt   # Setting font for the graph
+#        plt.rcParams["font.family"] = "Quick Sand" 
+
         fig, grp = plt.subplots()
         grp.annotate(Num, xy = ([3,3.2]), size = 100, weight='bold', color = color)    
         if Heading == 'Body Mass Index' or Heading == 'Body Fat':       
             grp.annotate(Unit, xy = ([6.5,3.5]), size=15)
         else:
-            grp.annotate(Unit, xy = ([7,3.5]), size=15)
-        
-        plt.title(Heading, y=0.85, size = 20)
+            grp.annotate(Unit, xy = ([7,3.5]), size=15)     
+#        plt.title(Heading, y=0.85, size = 20, )
+#        plt.rcParams["font.family"] = "Quick Sand" 
+        plt.annotate(Heading, xy= ([2.5,8]),size=20, weight='bold')
+
         plt.ylim([0,10])
         plt.xlim([0,10])
         plt.axis('off')
         # Making a circle with the centre (5,5) and radius 3
         circle1 = plt.Circle((5,5), 3.2, color = 'k',alpha = 0.05)
-        grp.add_artist(circle1)
+#        grp.add_artist(circle1)
         plt.savefig(Heading +".png", dpi = 400)
         plt.grid()
         plt.show()
@@ -38,6 +42,53 @@ num_graph("Health Score", 98 , "", midnight_blue[5])
 num_graph("Height", 170 , "cm", emerald[8])    
 num_graph("Body Mass Index", 23 , "kg/m\N{SUPERSCRIPT Two}", sunflower[8])    
 num_graph("Body Fat", 20 , "%" , pomegranate[8])    
+
+# Graph for when you eat
+iBLSD = [25,30,15,30]
+aBLSD = [5,40,15,40]
+
+
+
+
+
+# Graph for what you eat
+def Food_Comp(C_P_F):
+    if __name__ == '__main__':
+        import matplotlib.pyplot as plt
+        plt.rcParams["font.family"] = 'Times New Roman'
+        plt.rc('grid', color='black', alpha = 0.075)
+        
+        plt.fill_between([1,3],45,65,color = emerald[3])
+        plt.annotate("Carb", xy = ([1,1]), color = midnight_blue[9], size = 20)
+        plt.plot([1,3],[C_P_F[0],C_P_F[0]],'-k', 3,C_P_F[0],'<k')
+        plt.annotate(str(C_P_F[0]),xy=([2,C_P_F[0]+ 2]),size=20,color=asbestos[8],weight='bold')
+        
+        plt.fill_between([4,6],10,35,color = emerald[3])
+        plt.annotate("Protein", xy = ([4,1]), color = midnight_blue[9], size = 20)
+        plt.plot([4,6],[C_P_F[1],C_P_F[1]],'-k', 6,C_P_F[1],'<k')
+        plt.annotate(str(C_P_F[1]),xy=([5,C_P_F[1]+ 2]),size=20,color=asbestos[8],weight='bold')
+        
+        plt.fill_between([7,9],15,25,color = emerald[3])
+        plt.annotate("Fat", xy = ([7,1]), color = midnight_blue[9], size = 20)
+        plt.plot([7,9],[C_P_F[2],C_P_F[2]],'-k', 9,C_P_F[2],'<k')
+        plt.annotate(str(C_P_F[2]),xy=([8,C_P_F[2]+ 2]),size=20,color=asbestos[8],weight='bold')
+        
+        plt.ylim([0,80])
+        plt.xlim([0,10])
+        plt.annotate("Ideal", xy = ([10,80]))
+        plt.xticks([])
+        plt.grid()
+        plt.savefig("Macro.png", dpi=400)
+        plt.show()
+
+C_P_F = [65,10,25]    
+Food_Comp(C_P_F)
+
+
+
+
+
+
 
 
 
--- Page1.py
+++ Page1.py
@@ -1,36 +1,33 @@
+from IPython import get_ipython
+get_ipython().magic('reset -sf')
 from fpdf import FPDF
-from webcolors import name_to_rgb
-#importing data from Child Model
-from CMC_20SEP import plot_BW,plot_FM,plot_FM_id,plot_H,Total_carb,Total_Fat,Total_Prot,Totalt,plot_age,plot_status,plot_status_id,plot_FMP,plot_FMP_id,plot_BMI,plot_BMI_id,plot_H_id
-
 pdf=FPDF(format='letter',unit='in')
+
+global epw , width, height
+width = pdf.w
+height = pdf.h
+epw = pdf.w - 2*pdf.l_margin
 
 #New Page With Boarder
 def newpg():
     pdf.add_page()
-    global left
-    left = pdf.l_margin
-    global right
-    right = pdf.r_margin
-    global top
-    top = pdf.t_margin
-    global bottom
-    bottom = pdf.b_margin
-    pdf.ln(0.15)
+    pdf.set_margins(0.75, 1, 0.5)
 
-    global epw
-    epw = pdf.w - left - right
-    global eph
-    eph = pdf.h - top -bottom
+    pdf.set_font('times','I',18)
+    pdf.text(1,0.85, "{}'s Health Report".format(Name))
+    pdf.set_font('times','UI',12)
+    pdf.text(1,height-0.7, "Doctor's Comment:-")
+#    pdf.line(1,height-0.7,2,height-0.7)
+#    pdf.line(1,height-0.5,2,height-0.5)
+#    pdf.line(1,height-0.5,2,height-0.5)
+#    pdf.multi_cell(0,0, "{}'s Health Report".format(Name))
+#    pdf.multi_cell(0,9, "Doctor's comment".format(Name))
+    pdf.image(logo, x= 6.2 , y=0.3, w=2 , h= 0.5)
 
-    pdf.rect(left, top, w=epw, h=eph)
-    #ybefore=pdf.get_y()
-    #pdf.set_xy(epw/3+left, ybefore)
-    pdf.image(logo,x=6.2 , y=10.3, w=2 , h= 0.5, type ='JPEG')
-    #pdf.set_xy(left, ybefore)
+#    pdf.rect(0.75,1,width-1,height-2)        # (pdf.rect(left,top,right,bottom))
+    pdf.set_font('times','',14)
 
 # Variables
-
 Name = "Shreesha"
 Age = "24"
 Sex = "Male"
@@ -44,318 +41,68 @@ BW_PValue = "6454665"
 #Lean_Mass_RValue =" "
 
 #Images
+logo = "MetFlux_Logo.png"
+Body_Weight = "Body Weight_.png"
+Height = "Height_.png"
+BMI = "Body Mass Index_.png"
+Health_Score = "Health Score_.png"
+newpg()
+##============================================Page 1======================================#
+pdf.add_page()
+pdf.image(logo, x= 2.5 , y=0.1, w=4 , h= 0.75, )
+pdf.rect(0.75,1,width-1,height-2)
+pdf.set_font('times','I',20)
+pdf.text(2,1.5, "{}'s Health Report".format(Name))
 
-logo = "C:\\Users\\shreesha\\AppData\\Local\\Programs\\Python\\Python36\\New_L_J.jpg"
+x0 = -0.5; y0 = 1
+pdf.rect(1.75+x0,1.25+y0,4+x0,1.75+y0)       # (pdf.rect(left,top,right,bottom))
+pdf.set_font('times','',14)
+pdf.text(2+x0,1.75+y0, "Name:           Shreesha")
+pdf.text(2+x0,2.25+y0, "ID:               MFSH93")
+pdf.text(2+x0,2.75+y0, "Gender:             Male")
+pdf.text(2+x0,3.25+y0,  "DOB:         07/01/1993")
 
-BMI_pic= "C:\\Users\\shreesha\\AppData\\Local\\Programs\\Python\\Python36\\gauge.png"
-
-BPF_pic="C:\\Users\\shreesha\\Desktop\\MetFlux\\Fat percent.png"
-
-Weight_pic="C:\\Users\\shreesha\\Desktop\\MetFlux\\Weight.png"
-
-
-
-#Text Lines
-
-title = """
-Your Personalised Health Report:-
-
-"""
-
-int111 = """'If you could give every individual the right amount of \
-nourishment and exercise, not too little and not too much, then we have fond the\
- safest way to health.'"""
-
-intro2 = """Metflux is pleased to provide you with your personalized overall\
- health Report designed to help you discover a healthier and better you \
+#pdf.image(Health_Score,x=5.5,y=2.5, w=2,h=1.75)
+#pdf.ln(pdf.font_size)
+pdf.text(1.1,5.5,"Dear {}".format(Name))
+intro = """Metflux is pleased to provide you with your personalized overall \
+health Report designed to help you discover a healthier and better you\
 by providing recommendations to your daily diet based on cutting-edge \
-technology developed by our team.
+technology developed by our team.\
 Everything you eat and drink over time matters. \
-The right mix can help you be healthier now and in the future.
+The right mix can help you be healthier now and in the future.\
 Start with small changes to make healthier choices and enjoy life \
 keeping your body at its prime and we can help you get there!!"""
-
-Macro_Nutri = """Macronutrients are defined as a class of chemical compounds which humans consume in the \
-largest quantities and which provide humans with the bulk of energy. There are 3 major \
-Macronutrients Namely Carbohydrates, Proteins & Fats and a healthy diet includes a balance of \
-protein, carbohydrates and fats. Reducing or increasing any one of these nutrients \
-can have major consequences on the body."""
-
-Body_Comp = """Experimental investigation of human metabolism, nutrition, and body \
-composition over the past century has produced a wealth of quantitative data on how \
-the body dynamically adapts in response to diet changes and an imbalance between energy \
-intake and energy expenditure will lead to a change in body weight (mass) and body composition (fat and lean masses)."""
-
-BMI = """The BMI is an attempt to quantify the amount of tissue mass (muscle, fat, and bone) in an individual, \
-and then categorize that person as underweight, normal weight, overweight, or obese based on that value. \
-A healthy BMI is between 18.5 and 23. BMI is calculated by dividing your weight by the square of your height,\
-and is a general guide to let you know if you're within your healthy weight range.
-
-The generic classification is as shown below:-
-
-Underweight(<18) - Risk of developing problems such as nutritional deficiency and osteoporosis
-Normal(18-23) - Healthy Range
-Moderately Obese(23-28) - Moderate risk of developing heart disease, high blood pressure, stroke, diabetes.
-Severely Obese(>28) - High risk of developing heart disease, high blood pressure, stroke, diabetes
-
-
-"""
-
-BMI_Result = """For a normal healthy adult, the BMI ranges from 18-23 and your current score is at """+str(BMI_CValue)+""". Continuing \
-with your current lifestyle and diet, your score is predicted to be """+str(BMI_PValue)+""" after a year."""
-
-Body_Weight = """
-
-Body weight is the measurement of weight without items located on the person \
-and any Excess or reduction in the body weight is regarded as an indicator of determining a person's health, \
-with body volume measurement providing an extra dimension by calculating the distribution of body weight.
-
-
-
-
-"""
-
-Body_Weight_Result = """Considering that a normal healthy adult weigh around 47 to 60 kgs., \
-your current weight is """+str(BW_CValue)+""" kgs and is predicted to rise to """+str(BW_PValue)+""" kgs with the current lifestyle and diet."""
-
-BPF = """The body fat percentage (BFP) is the total mass of fat \
-divided by total body mass \
-where in body fat includes essential body fat and storage body fat. \
-The BPF is considered as \
-a fitness level measure as only body measurement calculates a persons \
-relative body composition \
-without regard to either height or body."""
-
-Lean_Muscle = """Lean muscle is related to lean body mass, which is the \
-content of the body without any fat. Lean body mass is used to calculate \
-basal metabolic rate. Lean muscle refers to muscle that is independent, \
-devoid of fat."""
-
-##Page 1
-
-newpg()
-
-##Code for boxes
-
-pdf.set_font('times','B',11)
-pdf.image(logo,x= None, y=None, w= epw, h= 2, type ='JPEG')
-pdf.ln()
-pdf.cell(0.9)
-pdf.cell(3,0.2,'Name: {}'.format(Name),border = 1 ,align = 'L')
-pdf.cell(3,0.2,'Age: {}'.format(Age),border = 1, align ='L')
-pdf.ln()
-pdf.cell(0.9)
-pdf.cell(3,0.2,'Gender: {}'.format(Sex),border = 1 ,align = 'L')
-pdf.cell(3,0.2,'Body Weight: {} kgs'.format(Weight),border = 1, align ='L')
-pdf.ln()
-pdf.cell(0.9)
-pdf.cell(3,0.2,'Height: {} cms'.format(Height),border = 1 ,align = 'L')
-pdf.cell(3,0.2,'Date: {}'.format(Date),border = 1, align ='L')
-pdf.ln(0.4)
-
-##Code for Intro
-
-pdf.set_font('times','',11)
-pdf.multi_cell(epw,0.15,int111)
-pdf.ln(0.15)
-pdf.set_font('times','B',11)
-pdf.cell(0,0,'Dear ABC,')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-pdf.multi_cell(epw,0.15,intro2)
-pdf.ln(0.25)
-
-##Code for Macro Nutrients
-
-pdf.set_font('times','B',14)
-pdf.set_fill_color(*name_to_rgb('black'))
-pdf.set_text_color(*name_to_rgb('white'))
-pdf.multi_cell(epw,0.15,title, fill= 1, align = 'C')
-pdf.set_text_color(*name_to_rgb('Black'))
-pdf.ln(0.25)
-pdf.set_font('times','B',12)
-pdf.cell(0,0,'Macronutrient Ratio')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-pdf.multi_cell(epw,0.15,Macro_Nutri)
-pdf.ln(0.15)
-
-##Page 2
-
-newpg()
-
-##Code for Body Composition
-
-pdf.set_font('times','B',12)
-pdf.cell(0,0,'Body Composition Dynamics')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-pdf.multi_cell(epw,0.15,Body_Comp)
-pdf.ln(0.25)
-
-##Code for BMI
-
-pdf.set_font('times','B',12)
-pdf.cell(0,0,'BMI- Body Mass Index')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-ybeforeBMI = pdf.get_y()
-pdf.multi_cell(epw/2,0.15,BMI)
-yafterBMI = pdf.get_y()
-hei = yafterBMI - ybeforeBMI
-pdf.set_xy(epw/2+left, ybeforeBMI)
-pdf.image(BMI_pic,x= None, y=None, w= epw/2, h= hei, type ='PNG')
-pdf.set_xy(left, yafterBMI)
-pdf.ln(0.1)
-pdf.set_font('times','B',12)
-pdf.set_fill_color(*name_to_rgb('lightgray'))
-pdf.multi_cell(epw,0.15,'Your BMI Test Result:-', fill= 1, align = 'C')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-pdf.set_fill_color(*name_to_rgb('white'))
-pdf.multi_cell(epw,0.15,BMI_Result,fill=1)
-pdf.ln(0.1)
-pdf.set_font('times','B',12)
-pdf.set_fill_color(*name_to_rgb('lightgray'))
-pdf.multi_cell(epw,0.15,' ', fill= 1, align = 'C')
-pdf.ln(0.25)
-##Code for Body Weight
-
-pdf.cell(0,0,'Body Weight')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-ybeforeBW = pdf.get_y()
-pdf.image(Weight_pic,x= None, y=None, w= epw/2, h= 2, type ='PNG')
-pdf.set_xy(epw/2+left,ybeforeBW)
-pdf.multi_cell(epw/2,0.15,Body_Weight)
-yafterBW = pdf.get_y()
-
-pdf.set_xy(left,yafterBW)
-pdf.ln(0.1)
-pdf.set_font('times','B',12)
-pdf.set_fill_color(229,229,229)
-pdf.multi_cell(epw,0.15,'Your Body Weight Test Result:-', fill= 1, align = 'C')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-pdf.multi_cell(epw,0.15,Body_Weight_Result)
-pdf.ln(0.1)
-pdf.set_font('times','B',12)
-pdf.set_fill_color(229,229,229)
-pdf.multi_cell(epw,0.15,' ', fill= 1, align = 'C')
-pdf.ln(0.25)
-
-##Page 3
-
-newpg()
-
-##Code for % fat
-
-pdf.set_font('times','B',12)
-pdf.cell(0,0,'BPF- Body Percentage Fat')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-ybeforeBPF = pdf.get_y()
-pdf.multi_cell(epw/2,0.15,BPF)
-yafterBPF = pdf.get_y()
-heig = yafterBPF - ybeforeBPF
-pdf.set_xy(epw/2+left, ybeforeBPF)
-pdf.image(BMI_pic,x= None, y=None, w= epw/2, h= heig, type ='PNG')
-pdf.set_xy(left, yafterBPF)
-pdf.ln(0.1)
-pdf.set_font('times','B',12)
-pdf.set_fill_color(*name_to_rgb('lightgray'))
-pdf.multi_cell(epw,0.15,'Your BPF Test Result:-', fill= 1, align = 'C')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-##pdf.set_fill_color(*name_to_rgb('yellow'))
-##pdf.multi_cell(epw,0.15,BMI_Result,fill=1)
-pdf.ln(0.1)
-pdf.set_font('times','B',12)
-pdf.set_fill_color(*name_to_rgb('lightgray'))
-pdf.multi_cell(epw,0.15,' ', fill= 1, align = 'C')
-pdf.ln(0.25)
-
-##Code for Lean Muscle
-
-pdf.cell(0,0,'Lean Muscle Mass')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-ybeforeLM = pdf.get_y()
-pdf.set_xy(epw/2+left,ybeforeLM)
-pdf.multi_cell(epw/2,0.15,Lean_Muscle)
-yafterLM = pdf.get_y()
-
-##Space for Pic
-
-pdf.set_xy(left,yafterLM)
-pdf.ln(0.1)
-pdf.set_font('times','B',12)
-pdf.set_fill_color(*name_to_rgb('lightgray'))
-pdf.multi_cell(epw,0.15,'Your Lean Muscle Mass Test Result:-', fill= 1, align = 'C')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-#pdf.multi_cell(epw,0.15,Body_Weight_Result)
-pdf.ln(0.1)
-pdf.set_font('times','B',12)
-pdf.set_fill_color(*name_to_rgb('lightgray'))
-pdf.multi_cell(epw,0.15,' ', fill= 1, align = 'C')
-pdf.ln(0.25)
-
-##page 4
-
-newpg()
-
-##New idea
-
-pdf.set_font('times','B',12)
-pdf.cell(0,0,'Title')
-pdf.ln(0.15)
-pdf.set_font('times','',11)
-pdf.multi_cell(epw/2,0.15,'Some Random Text')
-pdf.ln(0.15)
-pdf.set_font('times','B',12)
-pdf.set_fill_color(*name_to_rgb('lightgray'))
-pdf.multi_cell(epw,0.15,' ', fill= 1, align = 'C')
-ybef=pdf.get_y()
-pdf.ln(0.25)
-ybeforeF = pdf.get_y()
-pdf.cell(epw/2,0.15,'Your present day data')
-pdf.ln(0.2)
-pdf.image(BMI_pic,x= None, y=None, w= epw/2, h= 3, type ='PNG')
-ybeforez=pdf.get_y()
-pdf.ln(0.15)
-pdf.set_xy(left, ybeforez)
-pdf.set_font('times','',11)
-pdf.multi_cell(epw/2,0.15,'Some Random Text for Present data                       \
-dasdsadssa asdsdsadsdsad sdsdsdsfggth hthhdtheth thdthtdth htheththdhbth               ththbtbttb ththdggfb dffdgdfgtb dgbdbd,')
 pdf.ln(0.15)
 ylastP=pdf.get_y()
-pdf.set_xy(epw/2+left, ybeforeF)
-pdf.set_font('times','B',12)
-pdf.multi_cell(epw/2,0.15,'Your Future Data')
-ybeforex=pdf.get_y()
-pdf.ln(0.2)
-pdf.set_xy(epw/2+left, ybeforex)
-pdf.image(BMI_pic,x= None, y=None, w= epw/2, h= 3, type ='PNG')
-ybeforey=pdf.get_y()
-pdf.ln(0.15)
-pdf.set_xy(epw/2+left, ybeforey)
-pdf.set_font('times','',11)
-pdf.multi_cell(epw/2,0.15,'Some Random Textfor Future Data dsfsdfsdf sdfsdfsdf dfsdfsdf sdfsfsdf sfdfsdfdsf sdfsdfsdfsd sdfsdfdfsdf sdgsdgdsf dsfsdfdsfdsf')
-pdf.ln(0.15)
-ylastF=pdf.get_y()
-ylast=ylastP
-if ylastF > ylastP:
-    ylast = ylastF
-pdf.rect(epw/2+left, ybef, w= 0.01, h=ylast-ybef)
-pdf.set_xy(left, ylast)
-pdf.set_font('times','B',12)
-pdf.set_fill_color(*name_to_rgb('lightgray'))
-pdf.multi_cell(epw,0.15,'Your Title Test Result:-', fill= 1, align = 'C')
-pdf.ln(0.15)
+pdf.set_xy(1.1,5.65)
+pdf.set_font('times','',12)
+pdf.multi_cell(epw-0.75,0.25,intro)
+##============================================Page 2======================================#
 
+newpg()
+basic_details = [['Name', 'Gender', 'Age'],
+                 ['Shreesha', 'Male', '24'],
+                 ['ID','XYZ','ABC' ],
+                 ['CPnsk12', 'XYZ1','ABC1']]
+col_width = epw/3
+th = pdf.font_size
+#pdf.cell(epw, 0.0, 'Demographic data', align='C', )
+pdf.ln(0.75)
+for row in basic_details:
+    for r in row:
+#        pdf.cell(col_width,th, str(r),align ='C',border=1)
+        pdf.cell(col_width,2*th, str(r),align ='C',border=1)
+    pdf.ln(2*th)    
+#pdf.image(Body_Weight, x=2,y = 4, w=2.2,h=1.5)
+Text1 = "Your body weight is in healthy range. \
+          But you accumulated too much of fat"
 
+#pdf.text(4,4,Text1)
 
-##Code for Output
+pdf.multi_cell(7,0.5,Text1)
+pdf.image(Health_Score, x=5,y = 4, w=2.2,h=1.5)
+pdf.image(Height, x=2,y = 8, w=2.2,h=1.5)
+pdf.image(BMI, x=5,y = 8, w=2.2,h=1.5)
 
-pdf.output('Final_Report.pdf','F')
+pdf.output('Page_1.pdf','F')
